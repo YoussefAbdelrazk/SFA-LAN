0f568fd25b5964a58d0370253e69ff485b18b6cd
youseefmohamed <youssefabdelrazk2002@gmail.com>
2025-07-06 18:45:54 +0300

    Adding animation loader from react bits

--- src/components/ui/LayoutLoader.tsx
+++ src/components/ui/LayoutLoader.tsx
@@ -6,7 +6,7 @@ import Loader from "./Loader";
 const LayoutLoader = () => {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 2000);
+    const timer = setTimeout(() => setLoading(false), 3000);
     return () => clearTimeout(timer);
   }, []);
   return loading ? <Loader /> : null;
--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,24 +1,34 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import styles from "../css/Loader.module.css";
+import BlurText from "./BlureText";
 
 const WEBSITE_NAME = "SHERIF FRANCA";
 
 const Loader = () => {
-  const [displayed, setDisplayed] = useState("");
 
-  useEffect(() => {
-    let i = 0;
-    const interval = setInterval(() => {
-      setDisplayed(WEBSITE_NAME.slice(0, i + 1));
-      i++;
-      if (i === WEBSITE_NAME.length) clearInterval(interval);
-    }, 120); // Adjust speed here (ms per character)
-    return () => clearInterval(interval);
-  }, []);
+
+  // useEffect(() => {
+  //   let i = 0;
+  //   const interval = setInterval(() => {
+  //     setDisplayed(WEBSITE_NAME.slice(0, i + 1));
+  //     i++;
+  //     if (i === WEBSITE_NAME.length) clearInterval(interval);
+  //   }, 120); // Adjust speed here (ms per character)
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return (
     <div className={styles.loaderOverlay}>
-      <h1 className={styles.loaderText}>{displayed}</h1>
+     <BlurText
+  text={WEBSITE_NAME}
+  delay={180}
+  threshold={0.1}
+
+  animateBy="letters"
+  direction="top"
+
+  className="text-5xl mb-8 text-[#3E1492]"
+/>
     </div>
   );
 };
