b06df07959af676f4b77e4955b6a0d4376967cec
= <youssefabdelrazk2002@gmail.com>
2025-07-10 20:10:14 +0300

    change scroll icon in 3 language

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -94,7 +94,7 @@ export default function Hero() {
       {/* Scroll Indicator */}
       <div className='absolute bottom-8 left-1/2 transform -translate-x-1/2'>
         <div className='flex flex-col items-center space-y-2 text-white/60'>
-          <span className='text-sm font-medium'>Scroll to explore</span>
+          <span className='text-sm font-medium'>{t('scrollToExplore')}</span>
           <div className='w-6 h-10 border-2 border-white/30 rounded-full flex justify-center'>
             <div className='w-1 h-3 bg-white/60 rounded-full mt-2 animate-bounce'></div>
           </div>
